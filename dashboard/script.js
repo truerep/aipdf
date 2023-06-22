447f685392ab002942e04d8de91eb656d9caa64d
tradestealer999 <business.truerep@gmail.com>
2023-06-22 21:13:35 +0530

    --

--- dashboard/script.js
+++ dashboard/script.js
@@ -7,3 +7,13 @@ function closeSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.classList.remove('toggled');
 }
+
+function toggleThemeOptions() {
+    document.querySelector('.theme-options').classList.toggle('toggled');
+}
+ 
+function changeTheme(themeName) {
+    var themeStyle = document.getElementById('theme-style');
+    themeStyle.href = "./themes/"+themeName;
+    document.querySelector('.theme-options').classList.remove('toggled');
+}
