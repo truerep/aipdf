bb8e8987be250f2936c8a0149c99b160518db6cf
tradestealer999 <business.truerep@gmail.com>
2023-06-25 16:22:02 +0530

    dashboard page, table view, loader

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const fileInput = document.getElementById('file-input');
+    const fileNameSpan = document.getElementById('file-name');
+
+    fileInput.addEventListener('change', function() {
+        const file = fileInput.files[0];
+        if (file) {
+            fileNameSpan.textContent = file.name;
+        } else {
+            fileNameSpan.textContent = 'Browse Pdf';
+        }
+    });
+});
+
 function toggleSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.classList.add('toggled');
